e0c26c068e27d99e8af23374c0b2876c9a30fc9f
Mohammed Usama <usamabhakrani@gmail.com>
2023-12-19 18:38:43 +0500

    Learning Interface

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var axios_1 = require("axios");
+var url = "https://jsonplaceholder.typicode.com/todos/1";
+axios_1.get(url).then(function (res) {
+  return console.log(res.data);
+});
