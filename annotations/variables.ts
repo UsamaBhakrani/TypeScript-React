447493bbc3f5eac9f555c14e99f5f2291dafd3e8
Mohammed Usama <usamabhakrani@gmail.com>
2023-12-20 18:40:55 +0500

    Variables

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -3,6 +3,23 @@ let speed: string = "Fast";
 let hasName: boolean = true;
 let nothingMuch: null = null;
 let nothing: undefined = undefined;
+
+// Built in Objects
 let now: Date = new Date();
 
+// Arrays
 let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+let myBooleans: boolean[] = [true, false, true];
+
+// Classes
+class Car {}
+
+let car: Car = new Car();
+
+// Object Literal
+let point: { x: number; y: number; z: number } = {
+  x: 10,
+  y: 20,
+  z: 30,
+};
