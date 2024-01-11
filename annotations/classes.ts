4ef26a92d0f4db516a091b359faed255b66c34f2
UsamaBhakrani <usamabhakrani@gmail.com>
2024-01-11 22:19:02 +0500

    constructors

--- annotations/classes.ts
+++ annotations/classes.ts
@@ -1,13 +1,26 @@
 class Vehicle {
-  protected drive(): void {
-    console.log("Chuga Chuga");
+  constructor(public color: string) {}
+
+  protected honk(): void {
+    console.log("boom");
   }
 }
+
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
+
 class Car extends Vehicle {
-  drive(): void {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+  private drive(): void {
     console.log("VROOM VROOM");
   }
-  protect(): void {
+  startDrive(): void {
     this.drive();
+    this.honk();
   }
 }
+
+const car = new Car(1,"blue");
+car.startDrive();
