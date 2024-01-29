c6ef58095babb14e442e3280008536aeb15c2193
Mohammed Usama <usamabhakrani@gmail.com>
2024-01-29 16:26:47 +0500

    type inference with generics

--- annotations/generics.ts
+++ annotations/generics.ts
@@ -21,3 +21,35 @@ export class ArrayofAnything<T> {
 
 const arrayOfAnything = new ArrayofAnything<string>(["a"]);
 const arrayOfAnything1 = new ArrayofAnything<number>([1]);
+const arrayOfAnything2 = new ArrayofAnything(["a"]);
+const arrayOfAnything3 = new ArrayofAnything([1]);
+
+const printStrings = (arr: string[]): void => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+};
+
+const printNumbers = (arr: number[]): void => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+};
+
+const printAnything = <T>(arr: T[]): void => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+};
+function printAnything1<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(["a", "b"]);
+printAnything<number>([1, 2, 3, 4]);
+
+// Type Inference
+printAnything(["a", "b"]);
+printAnything([1, 2, 3, 4]);
